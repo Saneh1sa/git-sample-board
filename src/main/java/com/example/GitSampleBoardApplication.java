8bc6875a85b6a7c9c825314750fb61953b6157d8
saneh1sa <yuta.sanehisa@rakus.co.jp>
2021-06-04 09:56:11 +0900

    最初のコミット

---
+++ src/main/java/com/example/GitSampleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleBoardApplication.class, args);
+	}
+
+}

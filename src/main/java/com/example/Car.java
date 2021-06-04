99a5310150c1047d8709c0cae9577889be63e41b
saneh1sa <yuta.sanehisa@rakus.co.jp>
2021-06-04 10:41:11 +0900

    Carクラス編集

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,5 +1,14 @@
 package com.example;
 
 public class Car {
-
+	private int speed = 100;
+	
+	/**
+	 * スピード加速.
+	 * 
+	 * @return スピードを50足す
+	 */
+	public int addSpeed() {
+		return speed += 50;
+	}
 }

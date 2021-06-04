1e60c8bdd2c642ca72f22491f89eac094ce76ab0
rks-ookuma <takahiro.okuma@rakus.co.jp>
2021-06-04 10:38:46 +0900

    [Car]スピードフィールド作成、CheckSpeed処理を実装

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,5 +1,14 @@
 package com.example;
 
 public class Car {
+	private int speed = 200;
 
+	/**
+	 * 現在のスピードを確認する.
+	 *
+	 * @return 現在のスピード
+	 */
+	public int checkSpeed() {
+		return this.speed;
+	}
 }
